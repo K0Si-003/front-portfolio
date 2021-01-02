0c02bbc2222052a7734c5fc84def16047178db0f
K0Si-003 <hugo.pioda@gmail.com>
2021-01-02 15:35:28 +0100

    start and menu

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
+import Header from '../Header/Header';
+import Main from '../Main/Main';
+import Footer from '../Footer/Footer';
 
 const App = () => {
   return (
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Home from './views/Home';
-import Projects from './views/Projects';
-import Contact from './views/Contact';
+import Home from '../../views/Home';
+import Projects from '../../views/Projects';
+import Contact from '../../views/Contact';
 
 const Main = () => {
   return (
