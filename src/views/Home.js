0ee4381617d2351331de93d34bbba2b5b24d3422
K0Si-003 <hugo.pioda@gmail.com>
2021-01-21 19:20:23 +0100

    home nav

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,44 +1,47 @@
 import './home.css';
 import React from 'react';
-import { Transition } from 'react-transition-group';
 import ReactFullpage from '@fullpage/react-fullpage';
 
 const Home = () => {
-  const anchors = ['home', 'about', 'project', 'contact'];
+  const anchors = ['home', 'projects', 'about', 'contact'];
   return (
     <main className='home'>
       <ReactFullpage
-        // fullpage options
-        licenseKey={'YOUR_KEY_HERE'}
         anchors={anchors}
         navigation
         navigationPosition={'left'}
-        scrollingSpeed={800} /* Options here */
+        slidesNavigation={false}
+        scrollingSpeed={1000}
         render={({ state, fullpageApi }) => {
           return (
             <ReactFullpage.Wrapper>
-              <div className='section'>
-                <Transition timeout={1000} in={true} appear>
-                  {(status) => (
-                    <div className={`text text-${status}`} id='section1'>
-                      <h1>
-                        Hugo <br />
-                        Pioda
-                      </h1>
-                      <p>Web Developer</p>
-                    </div>
-                  )}
-                </Transition>
-              </div>
-              <div className='section'>
-                <p>About me</p>
-                <button onClick={() => fullpageApi.moveSectionDown()}>
-                  Click me to move down
-                </button>
-              </div>
-              <div className='section'>
-                <p>My latest projects</p>
-              </div>
+              <section className='section'>
+                <div className='container'>
+                  <h1>
+                    Hugo <br />
+                    Pioda
+                  </h1>
+                  <p>Web Developer</p>
+                </div>
+              </section>
+              <section className='section'>
+                <div className='container'>
+                  <p>Latests projects</p>
+                  <button onClick={() => fullpageApi.moveSectionDown()}>
+                    Click me to move down
+                  </button>
+                </div>
+              </section>
+              <section className='section'>
+                <div className='container'>
+                  <p>About me</p>
+                </div>
+              </section>
+              <section className='section'>
+                <div className='container'>
+                  <p>Contact me</p>
+                </div>
+              </section>
             </ReactFullpage.Wrapper>
           );
         }}
